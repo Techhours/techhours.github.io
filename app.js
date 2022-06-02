de1a246987a38259ee726779b0673d13ea3f93fa
Johan Gabriel VARGAS <johangabriel.vargas@sii.fr>
2022-06-02 15:48:15 +0200

    Changement de style de popup, placement des deux premières popups avec leurs boutons

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const first_btn = document.querySelector('.first_button');
-const first_popup = document.querySelector('.first-popup');
-const fourth_btn = document.querySelector('.fourth_button');
-const fourth_popup = document.querySelector('.fourth-popup')
+const first_popup = document.querySelector('.first_popup');
+const second_btn = document.querySelector('.second_button');
+const second_popup = document.querySelector('.second_popup');
 const background = document.querySelector('.wrappert');
 
-const first_image = document.querySelector('.img-popup-1')
 
 
 let isVisible1 = false;
 let clicked1 = false;
-let isVisible4 = false;
-let clicked4 = false;
-let test = false;
+let block1 = false;
+let isVisible2 = false;
+let clicked2 = false;
+let block2 = false;
+
 
 
 background.addEventListener('click',  () => {
-    if (!clicked1 && test == false) { first_popup.classList.remove('is-visible'); isVisible1 = false; }
-    if (!clicked4) { fourth_popup.classList.remove('is-visible'); isVisible4 = false; }
+    if (!clicked1 && block1 == false) { first_popup.classList.remove('is-visible'); isVisible1 = false; }
+    if (!clicked2 && block2 == false) { second_popup.classList.remove('is-visible'); isVisible2 = false; }
 
     clicked1 = false;
-    clicked4 = false;
-    test = false;
+    clicked2 = false;
+    block1 = false;
+    block2 = false;
     
 });
 
@@ -30,12 +32,16 @@ first_btn.addEventListener('click', () => {
     isVisible1 ? first_popup.classList.add('is-visible') : first_popup.classList.remove('is-visible');
 });
 
-fourth_btn.addEventListener('click', () => {
-    clicked4 = true;
-    isVisible4 = !isVisible4;
-    isVisible4 ? fourth_popup.classList.add('is-visible') : fourth_popup.classList.remove('is-visible');
+second_btn.addEventListener('click', () => {
+    clicked2 = true;
+    isVisible2 = !isVisible2;
+    isVisible2 ? second_popup.classList.add('is-visible') : second_popup.classList.remove('is-visible');
 });
 
-first_image.addEventListener('click', () => {
-    test = true;
+first_popup.addEventListener('click', () => {
+    block1 = true;
+});
+
+second_popup.addEventListener('click', () => {
+    block2 = true;
 });
